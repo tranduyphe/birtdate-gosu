d68ce6b423d6d8a2068ee129b98af7f10b0cf1d5
tranhuuhoang1905 <tranhuuhoang190594@gmail.com>
2023-09-15 20:26:43 +0700

    commit

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <meta name="csrf-token" content="{{ csrf_token() }}">
-        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+        <meta content=" Share To Shine" name="description" />
         <meta content="Themesdesign" name="author" />
         <!-- App favicon -->
         <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
